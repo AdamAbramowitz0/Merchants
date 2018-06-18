edbdba1d55f2d05476cc26693462f99dc3031252
Peter <peter@Daniels-MacBook-Pro-2.local>
2018-06-18 15:03:58 -0400

    adding list file

---
+++ List.py
@@ -0,0 +1,22 @@
+input = raw_input
+
+total = input("what would you like, you can type furniture or tech or car parts or mattresses: ")
+
+if total == "furniture":
+        sectionFruniture = input("would you like a couch or a chair or a table")
+        if sectionFruniture == "couch":
+            print("we have couches from ikea")
+        if sectionFruniture == "chair":
+            print("we have chairs from ikea")
+        if sectionFruniture == "table":
+            print("we have tables from ikea")
+
+
+if total == "tech":
+    sectionTech = input("")
+
+if total == "car parts":
+    sectionCarParts = input("")
+
+if total == "mattresses":
+    sectionMattresses = input("")
